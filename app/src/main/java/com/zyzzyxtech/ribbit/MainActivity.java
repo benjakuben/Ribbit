e8be66f2cf4c59e9a76c9f4eac842b6f9093c9a1
Ken W. Alger <kenalger@comcast.net>
2014-12-26 22:12:59 -0800

    Login & Sign-Up Activities added Navigation with Back and Up. Finished the Starting the App section

--- app/src/main/java/com/zyzzyxtech/ribbit/MainActivity.java
+++ app/src/main/java/com/zyzzyxtech/ribbit/MainActivity.java
@@ -43,6 +43,8 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
 
         // Set up the action bar.
